f5d8d96c69035c7f5df247574a49f19f90faa4fc
mhegedusic <mhegedusic@gmail.com>
2024-11-21 19:41:50 +0100

    display changes on mobile

--- src/languageswitcher.js
+++ src/languageswitcher.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function LanguageSwitcher() {
   const { i18n } = useTranslation();
@@ -11,10 +10,34 @@ function LanguageSwitcher() {
   };
 
   return (
-    <div className='languageSwitch pe-2 text-primary '>
-      <button className='btn btn-space btn-md' onClick={() => changeLanguage('hr')}>HR</button>
-      <button className='btn btn-space btn-md' onClick={() => changeLanguage('de')}>DE</button>
-     </div>
+    <div className="languageSwitch pe-2 text-primary ">
+      <button
+        className="btn btn-space btn-md"
+        onClick={() => changeLanguage('hr')}
+      >
+        <img
+          src="https://i.ibb.co/tDrzztC/logo-croatia.png"
+          alt="logo-croatia"
+          border="0"
+          width="20px"
+          height="10px"
+        ></img>
+        HR
+      </button>
+      <button
+        className="btn btn-space btn-md"
+        onClick={() => changeLanguage('de')}
+      >
+        <img
+          src="https://i.ibb.co/fF5r5Q9/deutch-logo.png"
+          alt="deutch-logo"
+          border="0"
+          width="20px"
+          height="10px"
+        ></img>
+        DE
+      </button>
+    </div>
   );
 }
 
--- src/navbar.js
+++ src/navbar.js
@@ -7,12 +7,30 @@ function Navbar() {
   const { t } = useTranslation();
 
   return (
-    <div className='text-center '>
-      <a href='/'><img src='https://i.ibb.co/QM0Mg96/bedi-logo.jpg' alt='' className='logo'></img></a>
-      <ul className='d-inline-flex ms-5 list-unstyled'>
-      <li className='p-4'><Link className='text-decoration-none text-black fs-3' to="/">{t('menu.home')}</Link></li>
-      <li className='p-4'><Link className='text-decoration-none text-black fs-3' to="/about">{t('menu.about')}</Link></li>
-      <li className='p-4'><Link to="/contact" className='text-decoration-none text-black fs-3'>{t('menu.contact')}</Link></li>
+    <div className="text-center navigation">
+      <a href="/">
+        <img
+          src="https://i.ibb.co/QM0Mg96/bedi-logo.jpg"
+          alt=""
+          className="logo"
+        ></img>
+      </a>
+      <ul className="d-inline-flex ms-5 list-unstyled">
+        <li className="p-4">
+          <Link className="text-decoration-none text-black fs-3" to="/">
+            {t('menu.home')}
+          </Link>
+        </li>
+        <li className="p-4">
+          <Link className="text-decoration-none text-black fs-3" to="/about">
+            {t('menu.about')}
+          </Link>
+        </li>
+        <li className="p-4">
+          <Link to="/contact" className="text-decoration-none text-black fs-3">
+            {t('menu.contact')}
+          </Link>
+        </li>
       </ul>
     </div>
   );
