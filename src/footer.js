d1e5ca16e5df5568f6c9d860c9930090b51f4ffc
mhegedusic <mhegedusic@gmail.com>
2024-11-21 20:37:09 +0100

    mobile visual changes

--- src/footer.js
+++ src/footer.js
@@ -3,22 +3,35 @@ import './bedi.css';
 
 const Footer = () => {
   return (
-    <div className='footer'>
-    <footer>
-      <div className='icons d-sm-inline-flex'>
-      <a href="https://www.facebook.com/people/Namje%C5%A1taj-Fodor/61560944850350/" target='_blank'><img src="https://i.ibb.co/hWkXQjf/face-logo.webp" alt="facelogo"/></a>
-      <a href="https://www.facebook.com/people/Namje%C5%A1taj-Fodor/61560944850350/" target='_blank'><img src="https://i.ibb.co/1LyLYGK/instalogo.png" alt="instalogo" /></a>
-      <a href="mailto: ivanfodor777@gmail.com"><img src="https://i.ibb.co/6RHMdW7/maillogo.png" alt="maillogo" /></a>
-      </div>
-      <br />
-      <strong>Filip Bedi <br />
-      Filip - Küchen & Möbelmontage <br />
-      <a href="tel:+49 173 2989675">Tel: +49 173 2989675</a> <br />
-      <a href="mailto:bf-design@hotmail.com">bf-design@hotmail.com</a></strong>
-      
-    </footer>
+    <div className="footer">
+      <footer>
+        <div className="icons">
+          <p className="contactUs">Kontaktirajte nas</p>
+          <a href="https://www.facebook.com/fbmontage" target="_blank">
+            <img
+              src="https://www.freeiconspng.com/uploads/facebook-logo-3.png"
+              width="350"
+              alt="Facebook Logo HD PNG Pic"
+            />
+          </a>
+          <a href="mailto: bf-design@hotmail.com">
+            <img
+              src="https://i.ibb.co/sjFJ7h0/email.png"
+              alt="email"
+              border="0"
+            />
+          </a>
+        </div>
+        <br />
+        <strong>
+          Filip Bedi <br />
+          Filip - Küchen & Möbelmontage <br />
+          <a href="tel:+49 173 2989675">Tel: +49 173 2989675</a> <br />
+          <a href="mailto:bf-design@hotmail.com">bf-design@hotmail.com</a>
+        </strong>
+      </footer>
     </div>
   );
-}
+};
 
 export default Footer;
--- src/navbar.js
+++ src/navbar.js
@@ -17,17 +17,17 @@ function Navbar() {
       </a>
       <ul className="d-inline-flex ms-5 list-unstyled">
         <li className="p-4">
-          <Link className="text-decoration-none text-black fs-3" to="/">
+          <Link className="li_font" to="/">
             {t('menu.home')}
           </Link>
         </li>
         <li className="p-4">
-          <Link className="text-decoration-none text-black fs-3" to="/about">
+          <Link className="li_font" to="/about">
             {t('menu.about')}
           </Link>
         </li>
         <li className="p-4">
-          <Link to="/contact" className="text-decoration-none text-black fs-3">
+          <Link to="/contact" className="li_font">
             {t('menu.contact')}
           </Link>
         </li>
